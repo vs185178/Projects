d30b1dd2818414db046054eaa16a2d8fb6c287e2
Siraparapu, Vijaya Ganesh <vs185178@ncr.com>
2024-03-06 22:13:23 +0530

    WPF AnalogClock CustomControl

---
+++ WPF/CustomControls/AnalogClockControl/CustomControls/AnalogClock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace AnalogClockControl.CustomControls
+{
+    public class AnalogClock: Control
+    {
+        Line hourHand;
+        Line minuteHand;
+        Line secondHand;
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            hourHand = Template.FindName("PART_HourHand", this) as Line;
+            minuteHand = Template.FindName("PART_MinuteHand", this) as Line;
+            secondHand = Template.FindName("PART_SecondHand", this) as Line;
+            UpdateHandAngles();
+            base.OnApplyTemplate();
+        }
+
+        private void UpdateHandAngles()
+        {
+            hourHand.RenderTransform = new RotateTransform((DateTime.Now.Hour / 12.0) * 360, 0.5, 0.5);
+            minuteHand.RenderTransform = new RotateTransform((DateTime.Now.Minute / 60.0) * 360, 0.5, 0.5);
+            secondHand.RenderTransform = new RotateTransform((DateTime.Now.Hour / 60.0) * 360, 0.5, 0.5);
+        }
+    }
+}
