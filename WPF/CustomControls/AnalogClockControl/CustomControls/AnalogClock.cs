2f65fb6d17c6c542813d240063b61c3d1683862d
Siraparapu, Vijaya Ganesh <vs185178@ncr.com>
2024-03-06 22:33:03 +0530

    Dependency Property binding in cs file

--- WPF/CustomControls/AnalogClockControl/CustomControls/AnalogClock.cs
+++ WPF/CustomControls/AnalogClockControl/CustomControls/AnalogClock.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -30,6 +31,7 @@ namespace AnalogClockControl.CustomControls
             DependencyProperty.Register("ShowSeconds", typeof(bool), typeof(AnalogClock), new PropertyMetadata(true));
 
 
+
         static AnalogClock()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
@@ -40,6 +42,16 @@ namespace AnalogClockControl.CustomControls
             hourHand = Template.FindName("PART_HourHand", this) as Line;
             minuteHand = Template.FindName("PART_MinuteHand", this) as Line;
             secondHand = Template.FindName("PART_SecondHand", this) as Line;
+
+            Binding showSecondHandBinding = new Binding
+            {
+                Path = new PropertyPath(nameof(ShowSeconds)),
+                Source = this,
+                Converter = new BooleanToVisibilityConverter()
+            };
+
+            secondHand.SetBinding(VisibilityProperty, showSecondHandBinding);
+
             UpdateHandAngles();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = new TimeSpan(0, 0, 1);
